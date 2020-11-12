8ba19f0289892531e8adb3d8cef04333d2925877
Ashley Davis <ashley@codecapers.com.au>
2020-11-13 09:36:18 +1000

    Added minimum TypeScript typings for the Kanban board data.

--- src/convertor.ts
+++ src/convertor.ts
@@ -1,7 +1,23 @@
 //
 // Represents Kanban board data.
 //
+
+export interface ICardData {
+
+}
+
+export interface ILaneData {
+
+    id: string;
+
+    title: string;
+
+    cards: ICardData[];
+}
+
 export interface IBoardData {
+
+    lanes: ILaneData[];
 
 }
 
@@ -10,13 +26,13 @@ export interface IBoardData {
 //
 export function markdownAstToBoarddata(markdownAST: any): IBoardData {
 
-    const boardData: any = {
+    const boardData: IBoardData = {
         lanes: [],
     };
 
     for (let childIndex = 0; childIndex < markdownAST.children.length; childIndex += 2) {
         const columnNode = markdownAST.children[childIndex];
-        const lane: any = {
+        const lane: ILaneData = {
             id: columnNode.children[0].value,
             title: columnNode.children[0].value,
             cards: [],
@@ -24,5 +40,5 @@ export function markdownAstToBoarddata(markdownAST: any): IBoardData {
         boardData.lanes.push(lane);
     }
 
-    return boardData;
+    return boardData;   
 }
