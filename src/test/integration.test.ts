f96e7e0a222535acd1156fcd4ea1ddefff98259b
Ashley Davis <ashley@codecapers.com.au>
2021-02-21 13:12:02 +1000

    Added a function to deindent multiline JavaScript strings.

--- src/test/integration.test.ts
+++ src/test/integration.test.ts
@@ -1,6 +1,7 @@
 import * as unified from "unified";
 import * as markdown from "remark-parse";
 import { markdownAstToBoarddata } from "../convertor";
+import * as dedent from "dedent-js";
 
 // Converts from markdown to AST.
 const fromMarkdownProcessor = unified().use(markdown);
@@ -9,13 +10,13 @@ describe("integration", () => {
 
     it("an integration test", () => {
 
-        const testMarkdown = 
-`### Todo
+        const testMarkdown = dedent(
+            `### Todo
 
-- [ ] Work on the website ~3d #feat @john 2020-03-20  
-- [ ] Fix the homepage ~1d #bug @jane  
-- [ ] Sub-task or description  
-`;
+            - [ ] Work on the website ~3d #feat @john 2020-03-20  
+            - [ ] Fix the homepage ~1d #bug @jane  
+            - [ ] Sub-task or description`
+        );
 
         const markdownAST = fromMarkdownProcessor.parse(testMarkdown);
         const board = markdownAstToBoarddata(markdownAST, () => "ABCD");
